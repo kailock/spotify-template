fb66ff71a7d7528f107cb910599913e29a76ccce
kailock <kailock@uw.edu>
2015-11-08 17:07:22 -0800

    got modals to work

--- app.js
+++ app.js
@@ -1,29 +1,44 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-var myApp = angular.module('myApp', [])
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
+var myApp = angular.module('myApp', []);
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
+  $scope.audioObject = {};
   $scope.getSongs = function() {
-    $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
-      
+
+    $http.get(baseUrl + $scope.searchedTrack).success(function(response){
+     /* data =*/ $scope.tracks = response.tracks.items;
+      console.log(response);
     })
-  }
+
+  };
   $scope.play = function(song) {
     if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
-      return
+      $scope.audioObject.pause();
+      $scope.currentSong = false;
+      return;
     }
     else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause();
       $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
+      $scope.audioObject.play();
       $scope.currentSong = song
     }
   }
-})
+
+  $scope.stop = function(song) {
+    $scope.audioObject.pause();
+    $scope.currentSong = false;
+    return;
+  }
+
+  $scope.playlist = [];
+  $scope.addSong = function(song) {
+    console.log(song);
+    $scope.playlist.push({title: song.name, artist: song.artists[0].name});
+
+  }
+});
 
 // Add tool tips to anything with a title property
 $('body').tooltip({
